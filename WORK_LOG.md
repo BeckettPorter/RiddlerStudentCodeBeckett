b6f53d47e438f63d98f18eb043121134d457baf7
BeckettPorter <porterbeckett@gmail.com>
2024-08-23 21:03:00 -0700

    Finished assignment (mostly, didn't get 4th one working)

--- WORK_LOG.md
+++ WORK_LOG.md
@@ -1,9 +1,13 @@
 Name: YOUR NAME HERE
 
-| Date   |    Time    |                                                                    Update |
-|:-------|:----------:|--------------------------------------------------------------------------:|
-| Aug 22 |  8-8:30pm  | I started by thinking about my place in the universe.<br/>It was scary... |
-|        |            |                                                             And then I... |
+| Date   |      Time       |                                                                                                                                              Update |
+|:-------|:---------------:|----------------------------------------------------------------------------------------------------------------------------------------------------:|
+| Aug 22 |  1:30 - 2:10pm  |                                                                                                                                    Worked in class. |
+| Aug 23 | 5:20pm - 5:40pm |                                                                                                                        Worked on the second method. |
+| Aug 23 |    8:30pm -     |                                                                                                      Finished the third method, trying to do fourth |
+|        |                 |                                                                                                  but I somehow managed to convert the characters to |
+|        |                 |                                                                                                     a list of vectors like this: [-30, -100, -124]. |
+|        |    - 9:00pm     | After that I tried to convert the characters to bytes, then I got this and converted it to ints and then to chars but this definitely wasn't right. |
 
 
 To add a new row to the table, click into a cell and then hit shift-enter.
--- src/Riddler.java
+++ src/Riddler.java
@@ -1,17 +1,39 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /**
  * The Riddler:
  * A puzzle by Zach Blick
  * for Adventures in Algorithms
  * At Menlo School in Atherton, CA
  *
- * Completed by: YOUR NAME HERE
+ * Completed by: Beckett Porter, August 23rd 2024
  */
 public class Riddler {
 
     public String decryptOne(String encrypted) {
         String decrypted = "";
 
+        // Checks to see if the decimal value of the number converted to a letter
+        // would be over the 17th letter in the alphabet or not, then shifts it by 9.
+        for (int i = 0; i < encrypted.length(); i++)
+        {
+            int num = encrypted.charAt(i);
+            if (num > 65)
+            {
+                if (num >= 113) {
+                    num -= 17;
+                }
+                else {
+                    num += 9;
+                }
+            }
+
+            decrypted += (char) (num);
+        }
         // TODO: Complete the decryptOne() function.
+
+        System.out.printf(decrypted);
 
         return decrypted;
     }
@@ -19,7 +41,18 @@ public class Riddler {
     public String decryptTwo(String encrypted) {
         String decrypted = "";
 
+        while (encrypted.contains(" "))
+        {
+            // I found this parseInt method online and this converts ascii decimal values to chars
+            int num = Integer.parseInt(encrypted.substring(0, encrypted.indexOf(" ")));
+
+            decrypted += (char) num;
+            encrypted = encrypted.substring(encrypted.indexOf(" ") + 1);
+        }
+
         // TODO: Complete the decryptTwo() function.
+
+        System.out.printf(decrypted);
 
         return decrypted;
     }
@@ -27,13 +60,36 @@ public class Riddler {
     public String decryptThree(String encrypted) {
         String decrypted = "";
 
+        for (int i = 0; i < encrypted.length(); i += 8)
+        {
+            // I found this version of the parseInt method online, and it converts binary to the ascii decimal value.
+            int num = Integer.parseInt(encrypted.substring(i, i + 8), 2);
+
+            String str = String.valueOf(num);
+
+            decrypted += (char) Integer.parseInt(str);
+        }
         // TODO: Complete the decryptThree() function.
+
+        System.out.printf(decrypted);
 
         return decrypted;
     }
 
     public String decryptFour(String encrypted) {
         String decrypted = "";
+
+        for (int i = 0; i < encrypted.length(); i++)
+        {
+            // Tried to convert the special characters to bytes and then to ints and then finally to chars...
+            // this did not work lol.
+            byte[] arrayOfBites = (encrypted.substring(i, i + 1).getBytes());
+            for (byte b : arrayOfBites)
+            {
+                decrypted += (char) Math.abs(b);
+            }
+        }
+        System.out.println(decrypted);
 
         // TODO: Complete the decryptFour() function.
 
